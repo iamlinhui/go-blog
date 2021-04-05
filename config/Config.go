582aa8d08a85782e80d7464c40420af6e9864699
lynn <lynn@promptness.cn>
2021-04-06 02:08:30 +0800

    架子搭建

--- config/Config.go
+++ config/Config.go
@@ -17,7 +17,7 @@ var (
 )
 
 func init() {
-	file, err := ini.Load("config/conf.ini")
+	file, err := ini.Load("config/application.ini")
 	if err != nil {
 		fmt.Println("配置文件读取错误，请检查文件路径:", err)
 	}
