9c8ea2c033e4e2efac3d69e5d4b5293874468079
lynn <lynn@promptness.cn>
2021-04-05 17:18:52 +0800

    vue

--- routes/router.go
+++ routes/router.go
@@ -5,6 +5,9 @@ import (
 	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
 	"go-blog/controller"
+	"log"
+	"net/http"
+	"runtime/debug"
 	"time"
 )
 
@@ -19,7 +22,7 @@ func InitRouter() {
 
 	engine := gin.New()
 	engine.HTMLRender = buildRender()
-	engine.Use(gin.Recovery())
+	engine.Use(Recover)
 	engine.Use(cors.New(
 		cors.Config{
 			AllowAllOrigins:  true,
@@ -30,23 +33,49 @@ func InitRouter() {
 			MaxAge:           12 * time.Hour,
 		},
 	))
-
-	engine.Static("/", "web/dist")
+	engine.Static("/css", "web/dist/css")
+	engine.Static("/img", "web/dist/img")
+	engine.Static("/js", "web/dist/js")
 	engine.StaticFile("/favicon.ico", "web/dist/favicon.ico")
 
 	engine.GET("/", func(c *gin.Context) {
 		c.HTML(200, "web", nil)
 	})
-
 	/*
 		前端展示页面接口
 	*/
 	router := engine.Group("api/v1")
+
 	{
 		// 获取个人设置信息
 		router.GET("index/:id", controller.Index)
 	}
 
+	engine.NoRoute(func(context *gin.Context) {
+		// context.Redirect(302,"/") 重定向到主页
+		context.JSON(http.StatusOK, gin.H{
+			"code": 404,
+			"msg":  "请求路径不正确",
+		})
+	})
+
 	_ = engine.Run(":8321")
 
+}
+
+func Recover(c *gin.Context) {
+	defer func() {
+		if r := recover(); r != nil {
+			//打印错误堆栈信息
+			log.Printf("panic: %v\n", r)
+			debug.PrintStack()
+			//封装通用json返回
+			c.JSON(http.StatusOK, gin.H{
+				"code": 500,
+				"msg":  "服务器内部错误",
+			})
+		}
+	}()
+	//加载完 defer recover，继续后续接口调用
+	c.Next()
 }
---
+++ web/src/views/page/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <h3>{{ msg }}</h3>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data() {
+    return {
+      msg: "ddddddd"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+</style>
